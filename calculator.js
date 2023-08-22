51097ee344cd17f4a13420c50899292acae4d4b8
klelik <klelika1998@gmail.com>
2023-08-22 16:08:41 +0100

    image source fix

--- calculator.js
+++ calculator.js
@@ -1,5 +1,5 @@
 angular
-  .module("extensionCalculatorApp", ['ngAnimate'])
+  .module("extensionCalculatorApp", ["ngAnimate"])
   .controller("CalculatorController", function ($scope, $timeout) {
     const vm = this;
 
@@ -9,13 +9,13 @@ angular
     vm.selections = {
       house: null,
       extensions: [],
-      roofs: []
+      roofs: [],
     };
 
     vm.priceBreakdown = {
       house: 0,
       extension: 0,
-      roof: 0
+      roof: 0,
     };
 
     vm.houseTypes = [
@@ -23,32 +23,32 @@ angular
         name: "Detached",
         selected: false,
         imageUrl: "/assets/houses/3D-detached.webp",
-        price: 100
+        price: 100,
       },
       {
         name: "Semi-Detached",
         selected: false,
         imageUrl: "/assets/houses/3D-semi-detached.webp",
-        price: 100
+        price: 100,
       },
       {
         name: "End of Terrace",
         selected: false,
         imageUrl: "/assets/houses/3D-end-of-tarrace.webp",
-        price: 100
+        price: 100,
       },
       {
         name: "Terraced-Flat Back",
         selected: false,
         imageUrl: "/assets/houses/3D-Flatback.webp",
-        price: 100
+        price: 100,
       },
       {
         name: "Terraced - L Shaped",
         selected: false,
         imageUrl: "/assets/houses/3D-terraced-l-shape.webp",
-        price: 100
-      }
+        price: 100,
+      },
     ];
 
     vm.extensionTypes = [
@@ -56,26 +56,26 @@ angular
         name: "Rear Extension",
         selected: false,
         imageUrl: "/assets/extensions/extension-detached.png",
-        price: 200
+        price: 200,
       },
       {
         name: "Side & Rear Extension",
         selected: false,
         imageUrl: "/assets/extensions/extension-side-and-rear.webp",
-        price: 400
+        price: 400,
       },
       {
         name: "Side Extension",
         selected: false,
         imageUrl: "/assets/extensions/extension-Pitched.webp",
-        price: 200
+        price: 200,
       },
       {
         name: "Wraparound Extension",
         selected: false,
         imageUrl: "/assets/extensions/extension-wraparound.webp",
-        price: 300
-      }
+        price: 300,
+      },
     ];
 
     vm.roofTypes = [
@@ -83,38 +83,38 @@ angular
         name: "Flat Roof",
         selected: false,
         imageUrl: "/assets/roofs/Flat-back.webp",
-        price: 300
+        price: 300,
       },
       {
         name: "Crown Roof",
         selected: false,
         imageUrl: "/assets/roofs/Rear-extension.webp",
-        price: 300
+        price: 300,
       },
       {
         name: "Gable Roof",
         selected: false,
         imageUrl: "/assets/roofs/Gable-roof.webp",
-        price: 300
+        price: 300,
       },
       {
         name: "Pitched Roof",
         selected: false,
         imageUrl: "/assets/roofs/Pitched.webp",
-        price: 300
+        price: 300,
       },
       {
         name: "Pitched & Flat Roof",
         selected: false,
         imageUrl: "/assets/roofs/Side-and-rear.webp",
-        price: 300
+        price: 300,
       },
       {
         name: "Pitched (Hip) Roof",
         selected: false,
         imageUrl: "/assets/roofs/Pitched-hip.webp",
-        price: 300
-      }
+        price: 300,
+      },
     ];
 
     vm.finalSelections = [
@@ -134,9 +134,9 @@ angular
         extensionType: ["Rear Extension"],
         roofType: ["Flat Roof"],
         imageUrl: "/assets/final/flat-roof1.jpg",
-        price: "77,000"
+        price: "77,000",
       },
-      //detatched - wraparound 
+      //detatched - wraparound
       {
         name: "FLAT ROOF",
         description: "Large Wraparound Extension",
@@ -144,7 +144,7 @@ angular
         extensionType: ["Wraparound Extension"],
         roofType: ["Flat Roof"],
         imageUrl: "/assets/final/large-wraparound1.png",
-        price: "77,000"
+        price: "77,000",
       },
       {
         name: "CROWN ROOF",
@@ -153,7 +153,7 @@ angular
         extensionType: ["Wraparound Extension"],
         roofType: ["Crown Roof"],
         imageUrl: "/assets/final/large-wraparound2.png",
-        price: "120,000"
+        price: "120,000",
       },
       //Semi-detached - rear extension (except from rear no other extensions)
       {
@@ -163,7 +163,7 @@ angular
         extensionType: ["Rear Extension"],
         roofType: ["Flat Roof"],
         imageUrl: "/assets/final/rear1.png",
-        price: "115,000"
+        price: "115,000",
       },
       {
         name: "CROWN ROOF",
@@ -172,7 +172,7 @@ angular
         extensionType: ["Rear Extension"],
         roofType: ["Crown Roof"],
         imageUrl: "/assets/final/rear-crown1.png",
-        price: "120,000"
+        price: "120,000",
       },
       //here
       // flatBack - rear -all
@@ -183,7 +183,7 @@ angular
         extensionType: ["Rear Extension"],
         roofType: ["Pitched Roof"],
         imageUrl: "/assets/final/rear2.png",
-        price: "80,000"
+        price: "80,000",
       },
       {
         name: "GABLE ROOF",
@@ -192,7 +192,7 @@ angular
         extensionType: ["Rear Extension"],
         roofType: ["Gable Roof"],
         imageUrl: "/assets/final/rear-gable.png",
-        price: "83,000"
+        price: "83,000",
       },
       {
         name: "FLAT ROOF",
@@ -201,7 +201,7 @@ angular
         extensionType: ["Rear Extension"],
         roofType: ["Flat Roof"],
         imageUrl: "/assets/final/rear3.png",
-        price: "82,000"
+        price: "82,000",
       },
       {
         name: "CROWN ROOF",
@@ -210,9 +210,9 @@ angular
         extensionType: ["Rear Extension"],
         roofType: ["Crown Roof"],
         imageUrl: "/assets/final/rear-crown2.png",
-        price: "85,000"
+        price: "85,000",
       },
-      // End Terrace - Rear + side 
+      // End Terrace - Rear + side
       {
         name: "FLAT ROOF",
         description: "3m x 3m Rear Extension",
@@ -220,9 +220,9 @@ angular
         extensionType: ["Rear Extension"],
         roofType: ["Flat Roof"],
         imageUrl: "/assets/final/end-rear-flat1.png",
-        price: "67,000"
+        price: "67,000",
       },
-      ///////// 
+      /////////
       // End Terrace - side & rear - pitched and flat
       {
         name: "PITCHED & FLAT ROOF",
@@ -231,7 +231,7 @@ angular
         extensionType: ["Side & Rear Extension"],
         roofType: ["Pitched & Flat Roof"],
         imageUrl: "/assets/final/end-sideRear-pitchedFlat1.png",
-        price: "112,000"
+        price: "112,000",
       },
       {
         name: "PITCHED & FLAT ROOF",
@@ -240,7 +240,7 @@ angular
         extensionType: ["Side & Rear Extension"],
         roofType: ["Pitched & Flat Roof"],
         imageUrl: "/assets/final/end-sideRear-pitchedFlat2.png",
-        price: "95,000"
+        price: "95,000",
       },
       {
         name: "PITCHED & FLAT ROOF",
@@ -249,7 +249,7 @@ angular
         extensionType: ["Side & Rear Extension"],
         roofType: ["Pitched & Flat Roof"],
         imageUrl: "/assets/final/end-sideRear-pitchedFlat3.png",
-        price: "83,000"
+        price: "83,000",
       },
       //////////////////////////////
       // End Terrace - side - flat (3)
@@ -260,7 +260,7 @@ angular
         extensionType: ["Side Extension"],
         roofType: ["Flat Roof"],
         imageUrl: "/assets/final/end-side-flat1.png",
-        price: "80,000"
+        price: "80,000",
       },
       {
         name: "FLAT ROOF",
@@ -269,7 +269,7 @@ angular
         extensionType: ["Side Extension"],
         roofType: ["Flat Roof"],
         imageUrl: "/assets/final/end-side-flat2.png",
-        price: "69,000"
+        price: "69,000",
       },
       {
         name: "FLAT ROOF",
@@ -278,7 +278,7 @@ angular
         extensionType: ["Side Extension"],
         roofType: ["Flat Roof"],
         imageUrl: "/assets/final/end-side-flat3.png",
-        price: "68,000"
+        price: "68,000",
       },
       // End Terrace - side - pitched (3)
       {
@@ -288,7 +288,7 @@ angular
         extensionType: ["Side Extension"],
         roofType: ["Pitched Roof"],
         imageUrl: "/assets/final/end-side-pitched1.png",
-        price: "78,000"
+        price: "78,000",
       },
       {
         name: "PITCHED ROOF",
@@ -297,7 +297,7 @@ angular
         extensionType: ["Side Extension"],
         roofType: ["Pitched Roof"],
         imageUrl: "/assets/final/end-side-pitched2.png",
-        price: "68,000"
+        price: "68,000",
       },
       {
         name: "PITCHED ROOF",
@@ -306,9 +306,9 @@ angular
         extensionType: ["Side Extension"],
         roofType: ["Pitched Roof"],
         imageUrl: "/assets/final/end-side-pitched3.png",
-        price: "67,000"
+        price: "67,000",
       },
-      // End Terrace - wraparound - flat (3) 
+      // End Terrace - wraparound - flat (3)
 
       {
         name: "FLAT ROOF",
@@ -317,7 +317,7 @@ angular
         extensionType: ["Wraparound Extension"],
         roofType: ["Flat Roof"],
         imageUrl: "/assets/final/end-wraparound-flat1.png",
-        price: "118,000"
+        price: "118,000",
       },
       {
         name: "FLAT ROOF",
@@ -326,7 +326,7 @@ angular
         extensionType: ["Wraparound Extension"],
         roofType: ["Flat Roof"],
         imageUrl: "/assets/final/end-wraparound-flat2.png",
-        price: "100,000"
+        price: "100,000",
       },
       {
         name: "FLAT ROOF",
@@ -335,9 +335,9 @@ angular
         extensionType: ["Wraparound Extension"],
         roofType: ["Flat Roof"],
         imageUrl: "/assets/final/end-wraparound-flat3.png",
-        price: "87,000"
+        price: "87,000",
       },
-      // End Terrace - wraparound - gable (3) 
+      // End Terrace - wraparound - gable (3)
 
       {
         name: "GABLE ROOF",
@@ -346,7 +346,7 @@ angular
         extensionType: ["Wraparound Extension"],
         roofType: ["Gable Roof"],
         imageUrl: "/assets/final/end-wraparound-gable1.png",
-        price: "112,000"
+        price: "112,000",
       },
       {
         name: "GABLE ROOF",
@@ -355,7 +355,7 @@ angular
         extensionType: ["Wraparound Extension"],
         roofType: ["Gable Roof"],
         imageUrl: "/assets/final/end-wraparound-gable2.png",
-        price: "97,000"
+        price: "97,000",
       },
       {
         name: "GABLE ROOF",
@@ -364,9 +364,9 @@ angular
         extensionType: ["Wraparound Extension"],
         roofType: ["Gable Roof"],
         imageUrl: "/assets/final/end-wraparound-gable3.png",
-        price: "86,000"
+        price: "86,000",
       },
-      // End Terrace - wraparound - pitched(hip) (3) 
+      // End Terrace - wraparound - pitched(hip) (3)
       {
         name: "PITCHED (HIP) ROOF",
         description: "Large Wraparound Extension",
@@ -374,7 +374,7 @@ angular
         extensionType: ["Wraparound Extension"],
         roofType: ["Pitched (Hip) Roof"],
         imageUrl: "/assets/final/end-wraparound-hip1.png",
-        price: "112,000"
+        price: "112,000",
       },
       {
         name: "PITCHED (HIP) ROOF",
@@ -383,7 +383,7 @@ angular
         extensionType: ["Wraparound Extension"],
         roofType: ["Pitched (Hip) Roof"],
         imageUrl: "/assets/final/end-wraparound-hip2.png",
-        price: "97,000"
+        price: "97,000",
       },
       {
         name: "PITCHED (HIP) ROOF",
@@ -392,9 +392,9 @@ angular
         extensionType: ["Wraparound Extension"],
         roofType: ["Pitched (Hip) Roof"],
         imageUrl: "/assets/final/end-wraparound-hip3.png",
-        price: "85,000"
+        price: "85,000",
       },
-      // flatBack - rear -all - same as semi - detached, except one -> houseType:[ "Semi-Detached","Terraced-Flat Back"]      
+      // flatBack - rear -all - same as semi - detached, except one -> houseType:[ "Semi-Detached","Terraced-Flat Back"]
     ];
 
     vm.toggleSelection = function (item, list) {
@@ -405,19 +405,27 @@ angular
     vm.filteredSelections = angular.copy(vm.finalSelections);
 
     vm.updateFilteredSelections = function () {
-
-      const selectedHouse = vm.selections.house ? vm.selections.house.name : null;
-      const selectedExtensions = vm.selections.extensions.map(ext => ext.name);
-      const selectedRoofs = vm.selections.roofs.map(roof => roof.name);
+      const selectedHouse = vm.selections.house
+        ? vm.selections.house.name
+        : null;
+      const selectedExtensions = vm.selections.extensions.map(
+        (ext) => ext.name
+      );
+      const selectedRoofs = vm.selections.roofs.map((roof) => roof.name);
 
       console.log("Selected House:", selectedHouse);
       console.log("Selected Extensions:", selectedExtensions);
       console.log("Selected Roofs:", selectedRoofs);
 
-      vm.filteredSelections = vm.finalSelections.filter(item => {
-        const houseMatches = !selectedHouse || item.houseType.includes(selectedHouse);
-        const extensionsMatch = selectedExtensions.length === 0 || item.extensionType.some(ext => selectedExtensions.includes(ext));
-        const roofsMatch = selectedRoofs.length === 0 || item.roofType.some(roof => selectedRoofs.includes(roof));
+      vm.filteredSelections = vm.finalSelections.filter((item) => {
+        const houseMatches =
+          !selectedHouse || item.houseType.includes(selectedHouse);
+        const extensionsMatch =
+          selectedExtensions.length === 0 ||
+          item.extensionType.some((ext) => selectedExtensions.includes(ext));
+        const roofsMatch =
+          selectedRoofs.length === 0 ||
+          item.roofType.some((roof) => selectedRoofs.includes(roof));
 
         // Log for debugging
         // console.log("For item:", item.name);
@@ -429,16 +437,16 @@ angular
 
       // Checking the result
       console.log("Filtered Selections after update:", vm.filteredSelections);
-    }
+    };
 
-    //keep a copy so the arrays dont get mutated 
+    //keep a copy so the arrays dont get mutated
     vm.originalExtensionTypes = angular.copy(vm.extensionTypes);
     vm.originalRoofTypes = angular.copy(vm.roofTypes);
     function getPossibleExtensions(houseType) {
       let extensions = new Set(); // unique values only
       for (let item of vm.finalSelections) {
         if (item.houseType.includes(houseType)) {
-          item.extensionType.forEach(ext => extensions.add(ext));
+          item.extensionType.forEach((ext) => extensions.add(ext));
         }
       }
       return Array.from(extensions);
@@ -447,24 +455,29 @@ angular
     function getPossibleRoofs(houseType, extensionNames) {
       let roofs = new Set();
       for (let item of vm.finalSelections) {
-        if (item.houseType.includes(houseType) && extensionNames.some(extName => item.extensionType.includes(extName))) {
-          item.roofType.forEach(roof => roofs.add(roof));
+        if (
+          item.houseType.includes(houseType) &&
+          extensionNames.some((extName) => item.extensionType.includes(extName))
+        ) {
+          item.roofType.forEach((roof) => roofs.add(roof));
         }
       }
       return Array.from(roofs);
     }
 
     vm.selectHouseType = function (house) {
-      vm.houseTypes.forEach(h => h.selected = false);  // Deselect all
-      house.selected = true;  // Select clicked one
+      vm.houseTypes.forEach((h) => (h.selected = false)); // Deselect all
+      house.selected = true; // Select clicked one
       vm.selections.house = house;
       vm.priceBreakdown.house = house.price;
       vm.updateFilteredSelections();
-      // filter down 
+      // filter down
       if (vm.selections.house) {
         let houseName = vm.selections.house.name;
         let possibleExtensions = getPossibleExtensions(houseName);
-        vm.extensionTypes = vm.originalExtensionTypes.filter(ext => possibleExtensions.includes(ext.name));
+        vm.extensionTypes = vm.originalExtensionTypes.filter((ext) =>
+          possibleExtensions.includes(ext.name)
+        );
       }
       console.log(vm.extensionTypes, "extensionTypes");
     };
@@ -484,9 +497,11 @@ angular
       // Filter down roof types based on selected house and extensions
       if (vm.selections.house && vm.selections.extensions.length) {
         let houseName = vm.selections.house.name;
-        let extensionNames = vm.selections.extensions.map(ext => ext.name);
+        let extensionNames = vm.selections.extensions.map((ext) => ext.name);
         let possibleRoofs = getPossibleRoofs(houseName, extensionNames);
-        vm.roofTypes = vm.originalRoofTypes.filter(roof => possibleRoofs.includes(roof.name));
+        vm.roofTypes = vm.originalRoofTypes.filter((roof) =>
+          possibleRoofs.includes(roof.name)
+        );
       }
       vm.updateFilteredSelections();
 
@@ -511,7 +526,7 @@ angular
       if (vm.currentStep < 4) {
         vm.currentStep++;
         updateProgressBar();
-        console.log(vm.selections, "selections")
+        console.log(vm.selections, "selections");
       }
     };
 
@@ -520,32 +535,32 @@ angular
         vm.currentStep--;
         updateProgressBar();
       }
-    }
+    };
 
     vm.goToStep = function (stepNumber) {
       if (vm.currentStep >= stepNumber) {
         vm.currentStep = stepNumber;
         // Adjust the progress width accordingly
-        updateProgressBar()
+        updateProgressBar();
       }
     };
 
     function updateProgressBar() {
       switch (vm.currentStep) {
         case 1:
-          vm.progressWidth = '33%';
+          vm.progressWidth = "33%";
           break;
         case 2:
-          vm.progressWidth = '66%';
+          vm.progressWidth = "66%";
           break;
         case 3:
-          vm.progressWidth = '100%';
+          vm.progressWidth = "100%";
           break;
         case 4:
-          vm.progressWidth = '100%';
+          vm.progressWidth = "100%";
           break;
         default:
-          vm.progressWidth = '0%';
+          vm.progressWidth = "0%";
       }
     }
     updateProgressBar();
@@ -558,7 +573,7 @@ angular
 
     //for loader
     vm.isLoading = false;
-    $scope.$watch('vm.filteredSelections', function (newVal, oldVal) {
+    $scope.$watch("vm.filteredSelections", function (newVal, oldVal) {
       if (newVal !== oldVal) {
         vm.isLoading = true;
         $timeout(function () {
@@ -568,20 +583,22 @@ angular
     });
 
     vm.findCostAndShowSelections = function () {
-      document.getElementById('selections').scrollIntoView({ behavior: 'smooth' });
-    }
+      document
+        .getElementById("selections")
+        .scrollIntoView({ behavior: "smooth" });
+    };
 
     vm.showAll = function () {
-      document.getElementById('all').scrollIntoView({ behavior: 'smooth' });
-    }
+      document.getElementById("all").scrollIntoView({ behavior: "smooth" });
+    };
 
     $scope.isElementInView = function (elem) {
       var docViewTop = $(window).scrollTop();
       var docViewBottom = docViewTop + $(window).height();
       var elemTop = $(elem).offset().top;
 
-      return (elemTop <= docViewBottom);
-    }
+      return elemTop <= docViewBottom;
+    };
 
     $(window).scroll(function () {
       $(".final-item").each(function () {
@@ -590,5 +607,4 @@ angular
         }
       });
     });
-
   });
